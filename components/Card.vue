70f4bb9ef1569902a597e8b45436eda4abc08ad3
Abulkosim Kataev <abulkasimkattaev@gmail.com>
2023-10-14 02:36:55 +0500

    unsplash api, img lists

--- components/Card.vue
+++ components/Card.vue
@@ -1,8 +1,8 @@
 
 <template>
   <div class="card-container">
-    <div class="card" v-for="card in cards">
-      <img :src="card.url">
+    <div class="card" v-for="src in cards">
+      <img :src="src">
     </div>
   </div>
 </template>
@@ -21,39 +21,24 @@
 }
 
 .card img:hover {
-  filter: brightness(70%)
+  filter: brightness(60%)
 }
 </style>
 
 <script setup>
-import { ref } from 'vue'
-const cards = ref([
-  {
-    url: 'https://images.unsplash.com/photo-1694456860373-9e89e3bd5903?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80'
-  },
-  {
-    url: 'https://images.unsplash.com/photo-1695112177760-1597b2c8eb5d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1364&q=80'
-  },
-  {
-    url: 'https://images.unsplash.com/photo-1695133389296-fdd6c49d422c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80'
-  },
-  {
-    url: 'https://images.unsplash.com/photo-1695071319752-371dcee7bb97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80'
-  },
-  {
-    url: 'https://plus.unsplash.com/premium_photo-1675826774815-35b8a48ddc2c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80'
-  },
-  {
-    url: 'https://images.unsplash.com/photo-1682687982093-4773cb0dbc2e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80'
-  },
-  {
-    url: 'https://images.unsplash.com/photo-1647891938250-954addeb9c51?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80'
-  },
-  {
-    url: 'https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80'
-  },
-  {
-    url: 'https://images.unsplash.com/photo-1433086966358-54859d0ed716?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80'
-  },
-])
+import { ref } from 'vue';
+
+const link = 'https://api.unsplash.com';
+const key = 'u9fK2wHosa-xHNkpqldYTnYZrDBiLObD3kCln1Vq7h8';
+const cards = ref([])
+
+async function unsplash() {
+  const response = await fetch(`${link}/photos?per_page=16&client_id=${key}&order_by=popular`);
+  const imgs = await response.json();
+  for (let img of imgs) {
+    cards.value.push(img.links.download)
+  }
+}
+unsplash()
+
 </script>
