756bff17d40682a7ae1b3ebf13c43cee09dfa458
Abulkosim Kataev <abulkasimkattaev@gmail.com>
2023-10-15 17:39:44 +0500

    firebase install

--- app.vue
+++ app.vue
@@ -2,8 +2,31 @@
   <Card />
 </template>
 
-<script setup lang="ts">
+<script setup>
 useHead({
   title: 'Images'
 })
+
+// import { initializeApp } from "firebase/app"
+// import { EmailAuthCredential, getAuth, onAuthStateChanged, signInWithPopup } from "firebase/auth";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBxVNQZTGmMJGChdMib1sm1A2U5yTPVOj4",
+//   authDomain: "images-be0b9.firebaseapp.com",
+//   projectId: "images-be0b9",
+//   storageBucket: "images-be0b9.appspot.com",
+//   messagingSenderId: "1085116140870",
+//   appId: "1:1085116140870:web:644a272b7026e1ebae2947",
+//   measurementId: "G-HZW27BD39N"
+// };
+
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+
+// onAuthStateChanged(auth, user => {
+//   console.log('You are logged in as ' + user)
+// })
+
+// signInWithPopup(auth, new EmailAuthCredential())
+
 </script>
