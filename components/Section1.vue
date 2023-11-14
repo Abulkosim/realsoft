a589cee7da9e058038dbf2b41635a81fc92249f9
Abulkosim Kataev <abulkasimkattaev@gmail.com>
2023-11-15 02:28:55 +0500

    header and main section

--- components/Section1.vue
+++ components/Section1.vue
@@ -2,9 +2,51 @@
   <div class="relative min-h-screen w-full bg-cover bg-no-repeat bg-center"
     style="background-image: url('https://images.unsplash.com/photo-1502086223501-7ea6ecd79368')">
     <div
-      class="absolute top-0 left-0 bottom-0 right-0 h-full w-full overflow-hidden bg-fixed bg-gradient-to-r from-[#3D00FF] to-[#AE13E2] opacity-80">
+      class="absolute top-0 left-0 bottom-0 right-0 h-full w-full overflow-hidden bg-fixed bg-gradient-to-r from-[#3D00FF] to-[#AE13E2] opacity-80 text-white">
+
+      <div class="max-w-6xl mx-auto h-24 flex items-center justify-between">
+        <div class="flex items-center gap-2 cursor-pointer select-none">
+          <img src="../assets/images/logo.svg" alt="Logo">
+          <div class="flex flex-col">
+            <span class="text-2xl font-sans">BOLALAR BOG'CHASI</span>
+            <span>Axborot tizimi</span>
+          </div>
+        </div>
+
+        <div class="flex items-center gap-6">
+          <span>Tizim haqida</span>
+          <span>Loyihalar</span>
+          <span>Qo'llanma</span>
+          <span>Aloqa</span>
+          <div class="flex items-center justify-center gap-3 bg-[#FF8A00] hover:bg-[#FF8A00dd] rounded-3xl p-3 w-48">
+            <ph-user :size="24" />
+            <span>Tizimga kirish</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="max-w-6xl mx-auto flex items-center gap-14 py-10">
+        <div class="basis-1/2 flex flex-col gap-6">
+          <h2 class="text-4xl font-semibold">Maktabgacha Ta’lim Tashkilotlari monitoringi</h2>
+          <p>Holisticly procrastinate mission-critical convergence with reliable customer service. Assertively underwhelm
+            idea-sharing for impactful solutions.</p>
+
+          <div class="flex items-center gap-6">
+            <div class="flex items-center justify-center bg-[#FF8A00] hover:bg-[#FF8A00dd] rounded-3xl p-3 w-48">
+              <span>Batafsil</span>
+            </div>
+          </div>
+
+        </div>
+        <div class="basis-1/2 rounded-lg shadow-md">
+          <img src="../assets/images/image 2.png" alt="Image" class="rounded-lg">
+        </div>
+      </div>
+
+
     </div>
   </div>
 </template>
 <script setup>
+import { PhUser } from '@phosphor-icons/vue';
 </script>
--- components/Section4.vue
+++ components/Section4.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="w-full h-40 bg-white"></div>
+  <div class="max-w-6xl mx-auto pt-20 pb-32 flex items-center gap-14 bg-white">
+
+    <div class="basis-1/2 flex flex-col gap-4">
+      <h3 class="text-[#0047FF] font-semibold">TIZIMNIK AFZALLIKLARI</h3>
+      <h2 class="text-3xl font-semibold">Bu tizim quyidagi afzalliklarga ega</h2>
+      <p v-for="item in 2" class="text-[#8F939D]">
+        Est cillum officia magna Lorem sunt officia elit in mollit enim nostrud minim. Et anim cillum ea exercitation
+        adipisicing consectetur enim proident tempor pariatur velit voluptate sunt. Minim laboris qui ullamco consequat
+        mollit consectetur adipisicing sint cupidatat anim ex aute. Laborum Lorem nostrud anim magna aute enim ullamco
+        nulla tempor anim tempor pariatur nulla commodo.
+      </p>
+    </div>
+    <div class="basis-1/2 grid grid-cols-2 gap-8">
+      <div class="border-[1px] border-gray-100 shadow-lg rounded-xl p-11 flex items-center flex-col gap-4 font-bold text-center">
+        <div class="w-20 h-20 bg-[#FF8A00] rounded-full"></div>
+        <span>Elektron shaklda ariza to’ldirish</span>
+      </div>
+      <div class="relative top-10 border-[1px] border-gray-100 shadow-lg rounded-xl p-11 flex items-center flex-col gap-4 font-bold text-center">
+        <div class="w-20 h-20 bg-[#FF8A00] rounded-full"></div>
+        <span>Elektron shaklda ariza to’ldirish</span>
+      </div>
+      <div class="border-[1px] border-gray-100 shadow-lg rounded-xl p-11 flex items-center flex-col gap-4 font-bold text-center">
+        <div class="w-20 h-20 bg-[#FF8A00] rounded-full"></div>
+        <span>Elektron shaklda ariza to’ldirish</span>
+      </div>
+      <div class="relative top-10 border-[1px] border-gray-100 shadow-lg rounded-xl p-11 flex items-center flex-col gap-4 font-bold text-center">
+        <div class="w-20 h-20 bg-[#FF8A00] rounded-full"></div>
+        <span>Elektron shaklda ariza to’ldirish</span>
+      </div>
+    </div>
+
+  </div>
 </template> 
 <script setup>
 </script>
