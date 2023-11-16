df9acb94e9e9e70071ee57fb760c229299db93cc
Abulkosim Kataev <abulkasimkattaev@gmail.com>
2023-11-16 21:16:40 +0500

    fixed mistakes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  target: "static",
-  router: {
-    base: "/realsoft/",
-  },
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
